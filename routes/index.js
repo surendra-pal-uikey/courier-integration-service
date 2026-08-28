cbc5f0c48b950517f78ba6d5f00c9fb84ab76843
surendra-pal-uikey <surendrapaluikey@gmail.com>
2026-08-28 18:01:31 +0530

    adding .evn sample file and change module type to es

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
-var express = require('express');
+import express from "express";
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.send({ messaage: "Hello world!!" });
 });
 
-module.exports = router;
+export default router;
